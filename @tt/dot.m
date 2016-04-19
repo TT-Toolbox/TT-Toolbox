476a3b32e70a3cb53ae46c235dd5c5d936990529
Sergey Dolgov <sd901@mapc-4050.campus.bath.ac.uk>
2016-04-19 12:30:48 +0100

    dot continued: works for subtensors and both border ranks>1

--- @tt/dot.m
+++ @tt/dot.m
@@ -3,13 +3,38 @@ function [c]=dot(a,b,varargin)
 %
 %   c = DOT(a,b) Inner product of two TT vectors (matrices are considered
 %   as nm x 1 vectors)
+%   c = DOT(a,b,dir,upto) Partial inner product over the cores from 1 to
+%   upto if dir=='LR' and from d to upto (inclusive) if dir=='RL'
 %
-%   See also: MTIMES, TIMES, PLUS, MINUS
+%   See also: MTIMES, TIMES, PLUS, MINUS, SUBTENSOR
 
 %   TT-Toolbox
 %   Copyright: TT-Toolbox team, 2016
 %   http://github.com/TT-Toolbox/TT-Toolbox
 %   BSD 2-clause license, see LICENSE
+
+
+if numel(varargin)>=2
+    % We have a partial dot product
+    dir = varargin{1};
+    upto = varargin{2};
+    if ~isa(upto, 'double')
+        error('The block index upto should be numeric');
+    end
+    
+    if strcmpi(dir, 'lr')
+        a = subtensor(a,1,upto);
+        b = subtensor(b,1,upto);
+    elseif strcmpi(dir,'rl')
+        a = subtensor(a,upto,a.d);
+        b = subtensor(b,upto,b.d);
+    else
+        error('dir should be either "lr" or "rl"');
+    end
+    
+    c = dot(a,b);
+    return;
+end
 
 [d,na,ra,a] = check_consistency(a);
 [d2,nb,rb,b] = check_consistency(b);
@@ -20,16 +45,39 @@ if any(prod(na,2) ~= prod(nb,2))
     error('Mode sizes of a and b are inconsistent');
 end
 
+if (ra(1)*rb(1) > 1) && (ra(d+1)*rb(d+1) > 1)
+    % All border ranks are >1, dot product is inefficient anyway, use *
+    warning('Both border ranks are not ones, using mtimes');
+    % Merge modes
+    a = cellfun(@(x)reshape(x, size(x,1), size(x,2)*size(x,3), 1, size(x,4)), a, 'UniformOutput', false);
+    a = tt(a);
+    b = cellfun(@(x)reshape(x, size(x,1), size(x,2)*size(x,3), 1, size(x,4)), b, 'UniformOutput', false);
+    b = tt(b);
+    
+    c = squeeze(full(a'*b));
+    return;
+end
+
 % Merge modes
 a = cellfun(@(x)reshape(x, size(x,1)*size(x,2)*size(x,3), size(x,4)), a, 'UniformOutput', false);
 b = cellfun(@(x)reshape(x, size(x,1), size(x,2)*size(x,3)*size(x,4)), b, 'UniformOutput', false);
 
-% c will be of size ra x rb
-c = 1;
-for i = 1:d
-    c = c*b{i};
-    c = reshape(c, ra(i)*na(i,1)*na(i,2), rb(i+1));
-    c = a{i}'*c;
-end
+c = 1; % c will be of size ra x rb
+
+if ra(1)*rb(1) == 1
+    % It's more efficient to multiply from the first block
+    for i = 1:d
+        c = c*b{i};
+        c = reshape(c, ra(i)*na(i,1)*na(i,2), rb(i+1));
+        c = a{i}'*c;
+    end
+elseif ra(d+1)*rb(d+1) == 1
+    % It's more efficient to multiply from the last block
+    for i = d:-1:1
+        c = conj(a{i})*c;
+        c = reshape(c, ra(i), na(i,1)*na(i,2)*rb(i+1));
+        c = c*b{i}.';
+    end    
+end;
 
 end
---
+++ @tt/subtensor.m
@@ -0,0 +1,21 @@
+function [b]=subtensor(a,istart,iend)
+% SUBTENSOR   part of a tensor train
+%
+%   b = SUBTENSOR(a,istart,iend) returns TT cores of a from istart to iend
+%   (inclusive)
+%
+%   See also: DOT
+
+%   TT-Toolbox
+%   Copyright: TT-Toolbox team, 2016
+%   http://github.com/TT-Toolbox/TT-Toolbox
+%   BSD 2-clause license, see LICENSE
+
+[d,~,~,b]=check_consistency(a);
+if (istart<1) || (istart>d) || (iend<1) || (iend>d)
+    error('istart and iend should be in a range from 1 to d');
+end
+b = b(istart:iend);
+b = tt(b);
+
+end
